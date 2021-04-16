023bfa34732139a02295eb15a15c639473c471f1
PaisMariano <mariano_a_p@hotmail.com>
2021-04-16 00:34:16 -0300

    orders relation finished, added mysql script

--- src/main/java/com/unq/edu/tpi/tip/backend/models/dtos/OrderDTO.java
+++ src/main/java/com/unq/edu/tpi/tip/backend/models/dtos/OrderDTO.java
@@ -1,6 +1,6 @@
 package com.unq.edu.tpi.tip.backend.models.dtos;
 
-import com.unq.edu.tpi.tip.backend.models.OrderedItem;
+import com.unq.edu.tpi.tip.backend.models.Item;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +13,5 @@ import java.util.Set;
 public class OrderDTO {
     private Long id;
     private Long tableId;
-    private Set<OrderedItem> orderedItems;
+    private Set<Item> orderedItems;
 }
