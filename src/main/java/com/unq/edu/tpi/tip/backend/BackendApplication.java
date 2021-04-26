e9ff3ae8fd9f76a4ad44b51b53d5ac0f05475da1
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-26 18:42:49 -0300

    added configuration to use mysql instead of h2

--- src/main/java/com/unq/edu/tpi/tip/backend/BackendApplication.java
+++ src/main/java/com/unq/edu/tpi/tip/backend/BackendApplication.java
@@ -2,11 +2,18 @@ package com.unq.edu.tpi.tip.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.unq.edu.tpi.tip.backend.*")
+@ComponentScan("com.unq.edu.tpi.tip.backend")
+@EntityScan(basePackages = "com.unq.edu.tpi.tip.backend.models")  // scan JPA entities manually
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
