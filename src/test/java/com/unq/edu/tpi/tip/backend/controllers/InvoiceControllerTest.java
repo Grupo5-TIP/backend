11eb7c6426076d824194a6b845f89a90e514ccc7
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-06-09 21:39:09 -0300

    raise coverage on invoice and ordertable controller

---
+++ src/test/java/com/unq/edu/tpi/tip/backend/controllers/InvoiceControllerTest.java
@@ -0,0 +1,55 @@
+package com.unq.edu.tpi.tip.backend.controllers;
+
+import com.unq.edu.tpi.tip.backend.mappers.OrderMapper;
+import com.unq.edu.tpi.tip.backend.models.dtos.InvoiceDTO;
+import com.unq.edu.tpi.tip.backend.models.dtos.OrderDTO;
+import com.unq.edu.tpi.tip.backend.services.InvoiceService;
+import com.unq.edu.tpi.tip.backend.services.OrderService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InvoiceControllerTest  extends TemplateControllerTest
+{
+
+	@InjectMocks
+	private InvoiceController invoiceController;
+
+	@Mock
+	private InvoiceService invoiceService;
+
+	@Before
+	public void setup()
+	{
+		mockMvc = MockMvcBuilders.standaloneSetup(invoiceController)
+				.build();
+	}
+
+	@Test
+	public void createInvoiceTest()  throws Exception{
+		InvoiceDTO invoiceDTO = new InvoiceDTO();
+		invoiceDTO.setPaymentType("MP");
+
+		MvcResult result = mockMvc.perform(
+				post("/api/invoices/1").contentType(MediaType.APPLICATION_JSON).content(asJsonString(invoiceDTO)))
+				.andExpect(status().isCreated())
+				.andReturn();
+
+		String response = result.getResponse().getContentAsString();
+		assertNotNull(response);
+	}
+
+}
